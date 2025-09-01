e6edc8897c6a15181e106f0fda62932b003f3188
Vaishanvi vijay shinde <vs8153042@gmail.com>
2025-09-01 19:39:08 +0530

    Create Divi.cpp

---
+++ Divi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {  
+            count++;
+        }
+    }
+    cout << "Total number of divisors: " << count << endl;
+    return 0;
+}
